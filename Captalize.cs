5e89fbca0ca4d5f486181daf3ab7dd43f7624146
Felipe Blini <felipe.blini@gmail.com>
2014-11-11 17:50:14 -0300

    Adicionado suporte para outros separadores além do ' '

--- Captalize.cs
+++ Captalize.cs
@@ -34,6 +34,8 @@ public static class Captalize
             char _prevCharMaisDois = ' ';
             char _nextCharMaisDois = ' ';
 
+            string _charMaisTres = "";
+            char _prevCharMaisTres = ' ';
             char _nextCharMaisTres = ' ';
 
             try
@@ -49,6 +51,7 @@ public static class Captalize
                 _charMaisDois = array[i] + "" + _nextChar + "" + array[i + 2];
                 _nextCharMaisDois = array[i + 3];
 
+                _charMaisTres = array[i] + "" + _nextChar + "" + array[i + 2] + "" + array[i + 3];
                 _nextCharMaisTres = array[i + 4];
             }
             catch
@@ -68,108 +71,111 @@ public static class Captalize
             if (new[] { "das", "dos", "nas", "nos" }.Contains(_charMaisDois) && _nextCharMaisDois == ' ' && _prevChar == ' ')
                 continue;
 
+            var separadores = new[] { ' ', ',', ';', '-', '_', ':', '/', '\\', '&', '$' };
+
             //algarismos romanos i, v e x sozinhos dá upper
-            if (!(_deuUpper) && new[] { 'i', 'v', 'x' }.Contains(_char) && _nextChar == ' ' && _prevChar == ' ')
+            if (!(_deuUpper) && new[] { 'i', 'v', 'x' }.Contains(_char) && separadores.Contains(_nextChar) && separadores.Contains(_prevChar))
                 array[i] = UpperCaracther(array[i]);
 
             if (_char == 'i')
             {
                 //primeiro i do ii
-                if (!(_deuUpper) && (_charMaisUm == "ii" && _nextCharMaisUm == ' ' && _prevChar == ' '))
+                if (!(_deuUpper) && (_charMaisUm == "ii" && separadores.Contains(_nextCharMaisUm) && separadores.Contains(_prevChar)))
                     array[i] = UpperCaracther(array[i]);
 
                 //primeiro i do iii
-                if (!(_deuUpper) && (_charMaisDois == "iii" && _nextCharMaisDois == ' ' && _prevCharMaisDois == ' '))
+                if (!(_deuUpper) && (_charMaisDois == "iii" && separadores.Contains(_nextCharMaisDois) && separadores.Contains(_prevCharMaisDois)))
                     array[i] = UpperCaracther(array[i]);
 
                 //segundo i do iii
-                if (!(_deuUpper) && (_nextChar == 'i' && _prevChar.ToString().ToLower() == "i" && (_prevCharMaisUm == ' '
-                    || (_prevCharMaisUm.ToString().ToLower() == "v" && _prevCharMaisDois == ' ')
-                    || (_prevCharMaisUm.ToString().ToLower() == "x" && _prevCharMaisDois == ' '))))
+                if (!(_deuUpper) && (_nextChar == 'i' && _prevChar.ToString().ToLower() == "i" && (separadores.Contains(_prevCharMaisUm)
+                    || (_prevCharMaisUm.ToString().ToLower() == "v" && separadores.Contains(_prevCharMaisDois))
+                    || (_prevCharMaisUm.ToString().ToLower() == "x" && separadores.Contains(_prevCharMaisDois)))))
                     array[i] = UpperCaracther(array[i]);
 
                 //ultimo i do iii ou ii
-                if (!(_deuUpper) && (_nextChar == ' ' && _prevChar.ToString().ToLower() == "i" && (_prevCharMaisUm == ' ' || _prevCharMaisDois == ' ')))
+                if (!(_deuUpper) && (separadores.Contains(_nextChar) && _prevChar.ToString().ToLower() == "i" && (separadores.Contains(_prevCharMaisUm) || separadores.Contains(_prevCharMaisDois))))
                     array[i] = UpperCaracther(array[i]);
 
                 //i antes do v ou x entre espacos em branco
-                if (!(_deuUpper) && ((_nextChar == 'v' || _nextChar == 'x') && _prevChar == ' ' && _nextCharMaisDois == ' '))
+                if (!(_deuUpper) && ((_nextChar == 'v' || _nextChar == 'x') && separadores.Contains(_prevChar) && separadores.Contains(_nextCharMaisDois)))
                     array[i] = UpperCaracther(array[i]);
 
                 //i depois do v ou x
-                if (!(_deuUpper) && ((_prevChar.ToString().ToLower() == "v" || _prevChar.ToString().ToLower() == "x") && _prevCharMaisUm == ' '
-                    && (_nextChar == ' ' || _nextCharMaisUm == ' ' || _nextCharMaisDois == ' ')))
+                if (!(_deuUpper) && ((_prevChar.ToString().ToLower() == "v" || _prevChar.ToString().ToLower() == "x") && separadores.Contains(_prevCharMaisUm)
+                    && (separadores.Contains(_nextChar) || separadores.Contains(_nextCharMaisUm) || separadores.Contains(_nextCharMaisDois))))
                     array[i] = UpperCaracther(array[i]);
             }
             else if (_char == 'l')
             {
                 //primeiro l do ll
-                if (!(_deuUpper) && (new[] { "ll" }.Contains(_charMaisUm) && _nextCharMaisUm == ' ' && _prevChar == ' '))
+                if (!(_deuUpper) && (new[] { "ll" }.Contains(_charMaisUm) && separadores.Contains(_nextCharMaisUm) && separadores.Contains(_prevChar)))
                     continue;
 
                 //primeiro l do lll
-                if (!(_deuUpper) && (new[] { "lll" }.Contains(_charMaisDois) && _nextCharMaisDois == ' ' && _prevCharMaisDois == ' '))
+                if (!(_deuUpper) && (new[] { "lll" }.Contains(_charMaisDois) && separadores.Contains(_nextCharMaisDois) && separadores.Contains(_prevCharMaisDois)))
                     continue;
 
                 //segundo l do lll
-                if (!(_deuUpper) && (_nextChar == 'l' && _prevChar.ToString().ToLower() == "l" && _prevCharMaisUm == ' '))
+                if (!(_deuUpper) && (_nextChar == 'l' && _prevChar.ToString().ToLower() == "l" && separadores.Contains(_prevCharMaisUm)))
                     continue;
 
                 //ultimo l do lll ou ll
-                if (!(_deuUpper) && (_nextChar == ' ' && _prevChar.ToString().ToLower() == "l" && (_prevCharMaisUm == ' ' || _prevCharMaisDois == ' ')))
+                if (!(_deuUpper) && (separadores.Contains(_nextChar) && _prevChar.ToString().ToLower() == "l" && (separadores.Contains(_prevCharMaisUm) || separadores.Contains(_prevCharMaisDois))))
                     continue;
 
                 //l antes de v ou x e entre espaços
-                if (!(_deuUpper) && ((_nextChar == 'v' || _nextChar == 'x') && _prevChar == ' ' && _nextCharMaisUm == ' '))
+                if (!(_deuUpper) && ((_nextChar == 'v' || _nextChar == 'x') && separadores.Contains(_prevChar) && separadores.Contains(_nextCharMaisUm)))
                     continue;
             }
             if (_char == 'v' || _char == 'x')
             {
                 //v ou x depois do i entre espacos em branco
-                if (!(_deuUpper) && (_nextChar == ' ' && _prevChar.ToString().ToLower() == "i"))
+                if (!(_deuUpper) && (separadores.Contains(_nextChar) && _prevChar.ToString().ToLower() == "i"))
                     array[i] = UpperCaracther(array[i]);
 
                 //vi, vii, viii
                 if (!(_deuUpper) &&
-                    ((_nextChar == 'i' && _nextCharMaisUm == ' ' && _prevChar == ' '))
-                    || (_nextChar == 'i' && _nextCharMaisUm == 'i' && _nextCharMaisDois == ' ' && _prevChar == ' ')
-                    || (_nextChar == 'i' && _nextCharMaisUm == 'i' && _nextCharMaisDois == 'i' && _nextCharMaisTres == ' ' && _prevChar == ' '))
+                    ((_nextChar == 'i' && separadores.Contains(_nextCharMaisUm) && separadores.Contains(_prevChar)))
+                    || (_nextChar == 'i' && _nextCharMaisUm == 'i' && separadores.Contains(_nextCharMaisDois) && separadores.Contains(_prevChar))
+                    || (_nextChar == 'i' && _nextCharMaisUm == 'i' && _nextCharMaisDois == 'i' && separadores.Contains(_nextCharMaisTres) && separadores.Contains(_prevChar)))
                     array[i] = UpperCaracther(array[i]);
 
                 //v ou x depois do l e entre espacos em branco
-                if (!(_deuUpper) && (_nextChar == ' ' && _prevChar.ToString().ToLower() == "l" && _prevCharMaisUm == ' '))
+                if (!(_deuUpper) && (_nextChar == ' ' && _prevChar.ToString().ToLower() == "l" && separadores.Contains(_prevCharMaisUm)))
                     array[i] = UpperCaracther(array[i]);
 
                 //vl, vll, vlll
                 if (!(_deuUpper) &&
-                    ((_nextChar == 'l' && _nextCharMaisUm == ' ' && _prevChar == ' '))
-                    || (_nextChar == 'l' && _nextCharMaisUm == 'l' && _nextCharMaisDois == ' ' && _prevChar == ' ')
-                    || (_nextChar == 'l' && _nextCharMaisUm == 'l' && _nextCharMaisDois == 'l' && _nextCharMaisTres == ' ' && _prevChar == ' '))
+                    ((_nextChar == 'l' && separadores.Contains(_nextCharMaisUm) && separadores.Contains(_prevChar)))
+                    || (_nextChar == 'l' && _nextCharMaisUm == 'l' && separadores.Contains(_nextCharMaisDois) && separadores.Contains(_prevChar))
+                    || (_nextChar == 'l' && _nextCharMaisUm == 'l' && _nextCharMaisDois == 'l' && separadores.Contains(_nextCharMaisTres) && separadores.Contains(_prevChar)))
                     array[i] = UpperCaracther(array[i]);
 
                 if (_char == 'v')
                 {
                     //v depois de x ou l precedido de espaço
-                    if (!(_deuUpper) && ((new[] { "x", "l" }.Contains(_prevChar.ToString().ToLower()) && (_prevCharMaisUm == ' ')) || ((new[] { "x", "l" }.Contains(_prevChar.ToString().ToLower()) && (_prevCharMaisUm == ' ' || _prevCharMaisDois == ' ')))))
+                    if (!(_deuUpper) && ((new[] { "x", "l" }.Contains(_prevChar.ToString().ToLower()) && (separadores.Contains(_prevCharMaisUm))) || ((new[] { "x", "l" }.Contains(_prevChar.ToString().ToLower()) && (separadores.Contains(_prevCharMaisUm) || separadores.Contains(_prevCharMaisDois))))))
                         array[i] = UpperCaracther(array[i]);
                 }
 
                 if (_char == 'x')
                 {
                     //x antes do iv entre espacos em branco
-                    if (!(_deuUpper) && (_nextChar == 'i' && _nextCharMaisUm.ToString().ToLower() == "v" && _nextCharMaisDois == ' ' && _prevChar == ' '))
+                    if (!(_deuUpper) && (_nextChar == 'i' && _nextCharMaisUm.ToString().ToLower() == "v" && separadores.Contains(_nextCharMaisDois) && separadores.Contains(_prevChar)))
                         array[i] = UpperCaracther(array[i]);
 
                     //x antes do lv entre espacos em branco
-                    if (!(_deuUpper) && (_nextChar == 'l' && _nextCharMaisUm.ToString().ToLower() == "v" && _nextCharMaisDois == ' ' && _prevChar == ' '))
+                    if (!(_deuUpper) && (_nextChar == 'l' && _nextCharMaisUm.ToString().ToLower() == "v" && separadores.Contains(_nextCharMaisDois) && separadores.Contains(_prevChar)))
                         array[i] = UpperCaracther(array[i]);
 
                     //x antes de v precedido de espaço
-                    if (!(_deuUpper) && (_nextChar == 'v' && _prevChar == ' '))
+                    if (!(_deuUpper) && (separadores.Contains(_nextCharMaisDois) && separadores.Contains(_prevChar)))
                         array[i] = UpperCaracther(array[i]);
                 }
             }
-            else if (_prevChar == ' ' && !_deuUpper)
+
+            if (separadores.Contains(_prevChar) && !_deuUpper)
                 array[i] = UpperCaracther(array[i]);
 
         }
